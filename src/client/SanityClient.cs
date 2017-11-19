18ad6f19adf51c023a53f019bc99949a1eabb7d0
Olav Nybø <olavnybo@gmail.com>
2017-11-19 22:42:03 +0100

    work in progress on the sanityclient

---
+++ src/client/SanityClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Olav.Sanity.Client
+{
+    public class SanityClient
+    {
+        private static readonly HttpClient HttpClient;
+
+        static SanityClient()
+        {
+            var handler = new HttpClientHandler();
+
+            HttpClient = new HttpClient(handler);
+            HttpClient.DefaultRequestHeaders.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public SanityClient(string projectId,
+                            string dataset,
+                            string token,
+                            bool useCdn)
+        {
+            
+        }
+
+        public object GetDocument(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Fetch(string query, string parms)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
