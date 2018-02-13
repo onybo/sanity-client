45caa4e950cd499528193b82a909e783f3affa5c
Olav Nybo <olavnybo@gmail.com>
2018-02-13 21:55:36 +0100

    Patch should not implement ISanityType

--- src/client/Mutators/PatchMutator.cs
+++ src/client/Mutators/PatchMutator.cs
@@ -1,15 +1,29 @@
+using System;
+using System.Reflection;
+
 namespace Olav.Sanity.Client.Mutators
 {
-    public class PatchMutator : Mutator
+    public class PatchMutator
     {
-        public ISanityType Patch 
+        private object _obj;
+        public object Patch
         {
             get => _obj;
-            set 
+            set
             {
                 RequireId(value);
                 _obj =value;
-            } 
+            }
+        }
+
+        private void RequireId(object type)
+        {
+            if (type is IHaveId)
+                return;
+            if (type.GetType().GetProperty("Id", BindingFlags.Public | BindingFlags.Instance, null, typeof(string), new Type[0], null) == null)
+            {
+                throw new Exception("Type does not implemented required public string property named Id");
+            }
         }
     }
 }
