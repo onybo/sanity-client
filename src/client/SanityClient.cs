e949a1a5c1d11018f1fb0028795989b1a22b2b97
Reng van Oord <reng.vanoord@oslofjord.com>
2018-05-28 13:18:25 +0200

    Reoorderd methods

--- src/client/SanityClient.cs
+++ src/client/SanityClient.cs
@@ -85,9 +85,22 @@ namespace Olav.Sanity.Client
             return (message.StatusCode, JsonConvert.DeserializeObject<T>(content));
         }
 
+        /// <summary>
+        /// Fetch documents using a GROQ query
+        /// </summary>
+        /// <param name="query">GROQ query</param>
+        /// <param name="excludeDrafts">set to false if unpublished documents should be included in the result</param>
+        /// <returns>Tuple of HttpStatusCode and T's wrapped in a FetchResult</returns>
+        public virtual async Task<(HttpStatusCode, FetchResult<T>)> Fetch<T>(string query, bool excludeDrafts = true) where T : ISanityDoc
+        {
+            var encodedQ = System.Net.WebUtility.UrlEncode(query);
+            var message = await _httpClient.GetAsync($"query/{_dataset}?query={encodedQ}").ConfigureAwait(false);
+            return await FetchResultToResult<FetchResult<T>, T>(message, excludeDrafts).ConfigureAwait(false);
+        }
+
         private async Task<(HttpStatusCode, T)> FetchResultToResult<T, V>(HttpResponseMessage message, bool excludeDrafts)
-                where T : FetchResult<V>
-                where V : ISanityDoc
+        where T : FetchResult<V>
+        where V : ISanityDoc
         {
             if (!message.IsSuccessStatusCode)
             {
@@ -101,19 +114,6 @@ namespace Olav.Sanity.Client
                                 result.Result;
 
             return (message.StatusCode, result);
-        }
-
-        /// <summary>
-        /// Fetch documents using a GROQ query
-        /// </summary>
-        /// <param name="query">GROQ query</param>
-        /// <param name="excludeDrafts">set to false if unpublished documents should be included in the result</param>
-        /// <returns>Tuple of HttpStatusCode and T's wrapped in a FetchResult</returns>
-        public virtual async Task<(HttpStatusCode, FetchResult<T>)> Fetch<T>(string query, bool excludeDrafts = true) where T : ISanityDoc
-        {
-            var encodedQ = System.Net.WebUtility.UrlEncode(query);
-            var message = await _httpClient.GetAsync($"query/{_dataset}?query={encodedQ}").ConfigureAwait(false);
-            return await FetchResultToResult<FetchResult<T>, T>(message, excludeDrafts).ConfigureAwait(false);
         }
 
         /// <summary>
