1d82e0970dde52fb3e9ce189ffaccb50373cf91a
Reng van Oord <reng.vanoord@oslofjord.com>
2018-05-28 11:02:05 +0200

    Suggested method renames

--- src/client/SanityClient.cs
+++ src/client/SanityClient.cs
@@ -63,12 +63,18 @@ namespace Olav.Sanity.Client
                 _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
         }
 
+        [Obsolete("Use GetDocumentAsync method instead.")]
+        public virtual Task<(HttpStatusCode, DocumentResult<T>)> GetDocument<T>(string id) where T : class
+        {
+            return GetDocumentAsync<T>(id);
+        }
+
         /// <summary>
         /// Get a single document by id
         /// </summary>
         /// <param name="id">Document id</param>
         /// <returns>Tuple of HttpStatusCode and a T wrapped in a DocumentResult</returns>
-        public virtual async Task<(HttpStatusCode, DocumentResult<T>)> GetDocument<T>(string id) where T : class
+        public virtual async Task<(HttpStatusCode, DocumentResult<T>)> GetDocumentAsync<T>(string id) where T : class
         {
             var message = await _httpClient.GetAsync($"doc/{_dataset}/{id}");
             return await ResponseToResult<DocumentResult<T>>(message);
@@ -103,13 +109,19 @@ namespace Olav.Sanity.Client
             return (message.StatusCode, result);
         }
 
+        [Obsolete("Use GetDocumentsAsync or QueryAsync method instead.")]
+        public virtual Task<(HttpStatusCode, FetchResult<T>)> Fetch<T>(string query, bool excludeDrafts = true) where T : ISanityDoc
+        {
+            return GetDocumentsAsync<T>(query, excludeDrafts);
+        }
+
         /// <summary>
         /// Fetch an array of documents using a GROQ query
         /// </summary>
         /// <param name="query">GROQ query</param>
         /// <param name="excludeDrafts">set to false if unpublished documents should be included in the result</param>
         /// <returns>Tuple of HttpStatusCode and T's wrapped in a FetchResult</returns>
-        public virtual async Task<(HttpStatusCode, FetchResult<T>)> Fetch<T>(string query, bool excludeDrafts = true) where T : ISanityDoc
+        public virtual async Task<(HttpStatusCode, FetchResult<T>)> GetDocumentsAsync<T>(string query, bool excludeDrafts = true) where T : ISanityDoc
         {
             var encodedQ = System.Net.WebUtility.UrlEncode(query);
             var message = await _httpClient.GetAsync($"query/{_dataset}?query={encodedQ}");
@@ -117,12 +129,12 @@ namespace Olav.Sanity.Client
         }
 
         /// <summary>
-        /// Fetch an arbitrary object result using a GROQ query. This should be used where the response is not necessarily 
-        /// expected to be an array. Typical examples include aggregate queries such as count() or queries for a single property or document.
+        /// Fetch an arbitrary query result using a GROQ query. This can be used where the response is not necessarily 
+        /// expected to be an array of documents. Typical examples include aggregate queries such as count() or queries for a single property or document.
         /// </summary>
         /// <param name="query">GROQ query</param>
         /// <returns>Tuple of HttpStatusCode and T's wrapped in a FetchResult</returns>
-        public virtual async Task<(HttpStatusCode, QueryResult<T>)> Query<T>(string query)
+        public virtual async Task<(HttpStatusCode, QueryResult<T>)> QueryAsync<T>(string query)
         {
             var encodedQ = System.Net.WebUtility.UrlEncode(query);
             var message = await _httpClient.GetAsync($"query/{_dataset}?query={encodedQ}").ConfigureAwait(false);
@@ -136,7 +148,7 @@ namespace Olav.Sanity.Client
             {
                 return (message.StatusCode, null);
             }
-            var content = await message.Content.ReadAsStringAsync();
+            var content = await message.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             var result = JsonConvert.DeserializeObject<T>(content);
 
@@ -151,7 +163,7 @@ namespace Olav.Sanity.Client
         /// <param name="returnIds">If true, the id's of modified documents are returned</param>
         /// <param name="returnDocuments">If true, the entire content of changed documents is returned</param>
         /// <param name="visibility">If "sync" the request will not return until the requested changes are visible to subsequent queries, if "async" the request will return immediately when the changes have been committed. For maximum performance, use "async" always, except when you need your next query to see the changes you made. "deferred" is used in cases where you are adding or mutating a large number of documents and don't need them to be immediately available.</param>
-        public virtual async Task<(HttpStatusCode, MutationResult)> Mutate(
+        public virtual async Task<(HttpStatusCode, MutationResult)> MutateAsync(
             Mutations mutations, bool returnIds = false, bool returnDocuments = false,
             Visibility visibility = Visibility.Sync)
         {
